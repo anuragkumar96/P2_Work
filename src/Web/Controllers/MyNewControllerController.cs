f83010b4fe4d4bacbcbdf2ad4273b9b2b99ac2ed
ANURAG KUMAR <s531372@nwmissouri.edu>
2019-02-08 15:53:00 -0600

    final test commit

---
+++ src/Web/Controllers/MyNewControllerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class MyNewControllerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult mytext()
+        {
+            return Content("text");
+        }
+        public IActionResult myjson()
+        {
+            return Json(new { name = "json",desc = "this is json" });
+        }
+        public IActionResult myhtml()
+        {
+            return Content("<htm> <body> this is html </body></html>","text/html");
+        }
+        public IActionResult myview()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,37 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Cube_WorksWithOneInteger()
         {
+            var expected = 8;
+            var a = 2;
+            
+            var actual = Cube(a);
+            Assert.Equal(expected,actual);
+        }
 
+        
+
+    
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(-10)]
+        [InlineData(0)]
+        public void Divisibleby10_WorksWithOneInput(int n1)
+        {
+            Assert.True(Divisibleby10(n1));
+        }
+         
+
+        int Cube(int x)
+        {
+            return x * x * x;
+        }
+
+        bool Divisibleby10(int value)
+        {
+            return value % 10 == 0;
         }
     }
 }
